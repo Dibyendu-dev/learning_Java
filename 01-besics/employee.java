37995afd0d4435b253a2c05fd685b11e904e1d58
ddas4548 <ddas4548@gmail.com>
2025-10-28 15:25:35 +0530

    first commit

---
+++ 01-besics/employee.java
@@ -0,0 +1,8 @@
+public class employee {
+
+    public static void main(String args[]) {
+        int a=9;
+        System.out.println("hello world, the value of a is:" + a);
+    }
+
+}
---
+++ 02-dataTypes/data.java
@@ -0,0 +1,10 @@
+public class data {
+    public static void main(String args[]) {
+            // char a = 88;
+            char a = 'a';
+            String mess ="Hello world";
+            System.out.println(a);
+            System.out.println(mess);
+
+    }
+}
